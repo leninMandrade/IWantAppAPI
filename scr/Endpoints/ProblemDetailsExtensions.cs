a9865ef0827b5e78e749095269b73631c773afac
Lênin Andrade <leninandrade.44@hotmail.com>
2022-10-15 20:27:17 -0300

    Adicionando autenticações, endpoint Employee e integrando o Dapper

--- scr/Endpoints/ProblemDetailsExtensions.cs
+++ scr/Endpoints/ProblemDetailsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using Microsoft.AspNetCore.Identity;
 using System.Runtime.CompilerServices;
 
 namespace IWantApp.Endpoints;
@@ -8,5 +9,13 @@ public static class ProblemDetailsExtensions
     public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notifications)
     {
         return notifications.GroupBy(g => g.Key).ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+    }
+
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IEnumerable<IdentityError> error)
+    {
+        var dictionary = new Dictionary<string, string[]>();
+        dictionary.Add("Error", error.Select(e => e.Description).ToArray());
+
+        return dictionary;
     }
 }
---
+++ scr/Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using IWantApp.Endpoints.Employees;
+using Microsoft.Data.SqlClient;
+
+namespace IWantApp.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+	private readonly IConfiguration configuration;
+	
+	public QueryAllUsersWithClaimName(IConfiguration configuration)
+	{
+		this.configuration = configuration;
+	}
+
+	public IEnumerable<EmployeeResponse> Execute(int page, int rows)
+	{
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+
+        var query = @"SELECT Email, ClaimValue as Name
+            from AspNetUsers u INNER JOIN AspNetUserClaims c
+            on u.id = c.UserId and ClaimType = 'Name'
+            order by Name
+            OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+        return db.Query<EmployeeResponse>(query, new { page, rows });
+    }
+}
